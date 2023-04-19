1dd670eaa048461555f1ea16d3f825c64807511a
VxleLUA <128989005+VxleLUA@users.noreply.github.com>
2023-04-18 21:16:08 -0400

    Create librarys.lua

---
+++ System/Storage/librarys.lua
@@ -0,0 +1,2 @@
+NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
