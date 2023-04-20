4b2af4dca915367d8504d396e1fa73863e59e65d
VxleLUA <128989005+VxleLUA@users.noreply.github.com>
2023-04-20 17:55:38 -0400

    Update Prefabs.lua

--- Modules/Prefabs.lua
+++ Modules/Prefabs.lua
@@ -1,25 +1,43 @@
+
+-- For anyone looking this is to keep a global updated discord link.
+local DiscordInvite = "https://raw.githubusercontent.com/your-username/your-repo/main/DiscordLink.lua"
+local DiscordContent = game:HttpGetAsync(DiscordInvite)
+local DiscordLink = loadstring(linkScriptContent)()
+
+-- Get global key.
+
+local GlobalStoredScriptHwid = loadstring(game:HttpGet("https://raw.githubusercontent.com/VxleLUA/Dynamic-Offical/main/Modules/Key.lua"))()
+
+-- For anyone looking through my scripts this is to randomize the names of each file so they dont override each config.
+local HwidFileGen = "hhttps://raw.githubusercontent.com/VxleLUA/Dynamic-Offical/main/Modules/HwidFile.lua"
+local linkScriptContent = game:HttpGetAsync(HwidFileGen)
+local GetContent = game:HttpGetAsync(HwidFileGen)
+local FileHwid = loadstring(GetContent)()
+
 local Window = Rayfield:CreateWindow({
    Name = "Dynamiq",
    LoadingTitle = "Loading...",
-   LoadingSubtitle = "powerd by rayfield",
+   LoadingSubtitle = "Please wait",
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
+      FolderName = nil,
+      FileName = "Dynamiq_" .. FileHwid,
    },
    Discord = {
       Enabled = false,
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+      Invite = "noinvitelink",
+      RememberJoins = true
    },
-   KeySystem = false, -- Set this to true to use our key system
+   KeySystem = true,
    KeySettings = {
       Title = "Untitled",
       Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided",
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Note = DiscordLink,
+      FileName = "Key",
+      SaveKey = false,
+      GrabKeyFromSite = false,
+      Key = {GlobalStoredHwid}
    }
 })
+
+local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
