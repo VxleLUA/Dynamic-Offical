d240d830bf9c165fd6755c10ea45300e2e158ff9
VxleLUA <128989005+VxleLUA@users.noreply.github.com>
2023-04-20 17:02:46 -0400

    Delete librarys.lua

--- System/Storage/librarys.lua
+++
@@ -1,2 +0,0 @@
-NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
