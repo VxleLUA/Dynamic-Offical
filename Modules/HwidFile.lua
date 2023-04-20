7d3d4040b633d56246878dc3de66336fa75e32ac
VxleLUA <128989005+VxleLUA@users.noreply.github.com>
2023-04-20 17:37:42 -0400

    Update HwidFile.lua

--- Modules/HwidFile.lua
+++ Modules/HwidFile.lua
@@ -1,1 +1,11 @@
+local function generateRandomString(length)
+   local characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+   local result = ""
+   for i = 1, length do
+      local randIndex = math.random(1, #characters)
+      result = result .. characters:sub(randIndex, randIndex)
+   end
+   return result
+end
 
+return generateRandomString(10)
