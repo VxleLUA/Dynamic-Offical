f68302fdc01ec3b00869b34f139c18c1f3a6eb71
VxleLUA <128989005+VxleLUA@users.noreply.github.com>
2023-04-21 00:27:44 -0400

    Update AutoFarm.lua

--- Modules/AutoFarm.lua
+++ Modules/AutoFarm.lua
@@ -8,3 +8,4 @@ wait(0.1)
 loadstring(game:HttpGet"https://raw.githubusercontent.com/VxleLUA/Dynamic-Offical/main/Modules/AutoFarmBeta/StartKill.lua")()
 wait(10)
 loadstring(game:HttpGet"https://github.com/VxleLUA/Dynamic-Offical/edit/main/Modules/AutoFarmBeta/ServerHop.lua")()
+-- ok
