0ad5f859e3156c56986727af8f3e0d9ff78da69c
VxleLUA <128989005+VxleLUA@users.noreply.github.com>
2023-04-26 21:32:19 -0400

    Update Preset.lua

--- System/Globals/Preset.lua
+++ System/Globals/Preset.lua
@@ -1,27 +1,27 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Rayfield Example Window",
-   LoadingTitle = "Rayfield Interface Suite",
-   LoadingSubtitle = "by Sirius",
+   Name = "Dynamiq Hub",
+   LoadingTitle = "Loading...",
+   LoadingSubtitle = "Please wait",
    ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
+      Enabled = false,
+      FolderName = nil,
+      FileName = "NIL"
    },
    Discord = {
-      Enabled = false,
+      Enabled = true,
       Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
-   KeySystem = false, -- Set this to true to use our key system
+   KeySystem = true, -- Set this to true to use our key system
    KeySettings = {
-      Title = "Untitled",
+      Title = "Dynamiq Hub",
       Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided",
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Note = "Join the discord!",
+      FileName = "Key",
+      SaveKey = false,
+      GrabKeyFromSite = false,
       Key = {"https://raw.githubusercontent.com/VxleLUA/Dynamic-Offical/main/System/Globals/Key.lua"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
