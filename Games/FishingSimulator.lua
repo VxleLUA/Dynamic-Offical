82660416ad749d4bd6921263690c32f9d1a1cea5
VxleLUA <128989005+VxleLUA@users.noreply.github.com>
2023-05-17 00:21:17 -0400

    Update FishingSimulator.lua

--- Games/FishingSimulator.lua
+++ Games/FishingSimulator.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("https://lilcoderscripts.online/Scripts/ProFisher"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VxleLUA/Dynamic-Offical/main/System/Notifications/GameSupported.lua"))()
+   wait()
+    loadstring(game:HttpGet("https://lilcoderscripts.online/Scripts/ProFisher"))()
