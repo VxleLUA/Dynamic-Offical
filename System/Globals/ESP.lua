1655472223382abb39dddb411f1f688d60b01b4d
VxleLUA <128989005+VxleLUA@users.noreply.github.com>
2023-05-19 03:13:32 -0400

    Update ESP.lua

--- System/Globals/ESP.lua
+++ System/Globals/ESP.lua
@@ -1,8 +1,8 @@
 -- 1. Load the library
 local Sense = loadstring(game:HttpGet('https://sirius.menu/sense'))()
   Sense.teamSettings.enemy.enabled = true
-    Sense.teamSettings.enemy.box3d = false
+    Sense.teamSettings.enemy.boxOutline = true
       Sense.teamSettings.enemy.healthBar = true
-        Sense.teamSettings.enemy.box3dColor = { Color3.new(), 1 },
+        Sense.teamSettings.enemy.boxOutlineColor = { Color3.new(), 1 }
  
 Sense.Load()
